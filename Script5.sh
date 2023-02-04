850bad6049f0120922638608724e0a5f61a41c88
Premkumar-Kadam <122383051+Premkumar-Kadam@users.noreply.github.com>
2023-02-04 18:09:41 +0530

    Update Script5.sh

--- Script5.sh
+++ Script5.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #This script is to make variable read only, means we cannot set the value of NAME variable again
-NAME=Young-Minds
+NAME=Warrior
 readonly NAME
-NAME=DEVOPS
+NAME=Premkumar
 echo "my name is: $NAME"
