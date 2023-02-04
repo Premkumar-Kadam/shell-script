9a131b012fe114c02d5c6c600d5eb14313c24f29
Premkumar-Kadam <122383051+Premkumar-Kadam@users.noreply.github.com>
2023-02-04 18:14:20 +0530

    Update Script4.sh

--- Script4.sh
+++ Script4.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #This is creation and calling of shell variable --- defining variables
-Class=Young-minds
-Batch=13
-PROFESSION=AWS/DevOps
-echo "Class Name is $Class, Batch number $Batch, We are learning $PROFESSION"
+Game=BGMI
+Rank=Conqueror
+PROFESSION= Rank Push
+echo "Game Name is $Game, Rank is $Batch, Doing  $PROFESSION"
